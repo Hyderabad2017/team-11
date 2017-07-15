a97f747be828febeff8277b97d6edc9d58e0ad73
uppala-sravani <sravanisunny1188@gmail.com>
2017-07-15 17:23:57 +0530

    registration module and validation

---
+++ Registration/app/src/main/java/com/example/sony/registration/registrationstudenttable.java
@@ -0,0 +1,13 @@
+package com.example.sony.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registrationstudenttable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrationstudenttable);
+    }
+}
