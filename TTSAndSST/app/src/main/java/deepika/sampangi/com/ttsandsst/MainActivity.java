fc15ff9a407bac06a11ca4e310c82c7b1276c996
DeepikaSampangi <deepikasampangi@gmail.com>
2017-07-16 07:33:14 +0530

    Trial for text to speech

---
+++ TTSAndSST/app/src/main/java/deepika/sampangi/com/ttsandsst/MainActivity.java
@@ -0,0 +1,17 @@
+package deepika.sampangi.com.ttsandsst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
---
+++ TTSAndSST/app/src/main/java/deepika/sampangi/com/ttsandsst/VolunteerHomePage.java
@@ -0,0 +1,13 @@
+package deepika.sampangi.com.ttsandsst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VolunteerHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_home_page);
+    }
+}
